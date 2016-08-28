f25f04095166f74f2c763c42fd28a7ed89490eab
Tom Elliott <tomelliottnz@gmail.com>
2016-08-29 10:21:47 +1200

    Created data package

---
+++ R/data.R
@@ -0,0 +1,203 @@
+#' Gapminder Dataset (2008)
+#'
+#' A dataset containing a variety of indiators from the Gapminder website,
+#' limited to the year 2008. Full details can be found on the website.
+#'
+#' @source \url{http://www.gapminder.org/data/}
+"gapminder_2008"
+
+#' Gapminder Dataset (1952 -- 2012)
+#'
+#' A dataset containing a variety of indiators from the Gapminder website,
+#' recorded every four years from 1952 to 2012.
+#' Full details can be found on the website.
+#'
+#' @source \url{http://www.gapminder.org/data/}
+"gapminder"
+
+#' NHANES Dataset (2009 -- 2012)
+#'
+#' A dataset adapted from the publicly available NHANES surveys from
+#' 2009--2012
+#' This is survey data collected by the US National Center for Health Statistics
+#' (NCHS) which has conducted a series of health and nutrition surveys since the
+#' early 1960's. Since 1999 approximately 5,000 individuals of all ages are
+#' interviewed in their homes every year and complete the health examination
+#' component of the survey. The health examination is conducted in a mobile
+#' examination centre (MEC).
+#'
+#' @source \url{http://www.cdc.gov/nchs/nhanes/nhanes_questionnaires.htm}
+"nhanes2009_2012"
+
+#' NHANES Dataset (First 50 rows)
+#'
+#' The first 50 rows of the NHANES data set.
+#'
+#' @source \url{http://www.cdc.gov/nchs/nhanes/nhanes_questionnaires.htm}
+"nhanes_1000_50"
+
+#' NHANES Dataset (First 1000 rows)
+#'
+#' The first 1000 rows of the NHANES data set.
+#'
+#' @source \url{http://www.cdc.gov/nchs/nhanes/nhanes_questionnaires.htm}
+"nhanes_1000"
+
+#' NHANES Dataset (First 2000 rows)
+#'
+#' The first 2000 rows of the NHANES data set.
+#'
+#' @source \url{http://www.cdc.gov/nchs/nhanes/nhanes_questionnaires.htm}
+"nhanes_2000"
+
+#' Olympics 100m
+#'
+#' Data from the 100m olympics event.
+"olympics100m"
+
+#' Canada Population
+#'
+#' This data is based on an experiment where the researcher asked 19
+#' students to estimate (in millions) the population of Canada
+#' (see Utts, J. M. & Heckard, R. F. (2014). Mind on Statistics,
+#' (5th ed.). USA: Cengage Learning.)
+"week7_CanadaPop"
+
+#' Cocaine Treatment Experiment
+#'
+#' From data from Gawin, F. et al (1989)
+#' Archives of General Psychiatry 46(2) (117-121)
+#' found in Statistics Unlocking the Power of Data (2013)
+#' by Lock, R.H. et al.
+"week7_CocaineTrtmtExpt"
+
+#' Cocaine Treatment Experiment (Lithum/Placebo)
+#'
+#' Only the Lithium and Placebo treatments.
+#'
+#' From data from Gawin, F. et al (1989)
+#' Archives of General Psychiatry 46(2) (117-121)
+#' found in Statistics Unlocking the Power of Data (2013)
+#' by Lock, R.H. et al.
+"week7_CocaineTrtmtExpt_2Trt"
+
+#' Cocaine Treatment Experiment (Desipramine/Placebo)
+#'
+#' Only the Desipramine and Placebo treatments.
+#'
+#' From data from Gawin, F. et al (1989)
+#' Archives of General Psychiatry 46(2) (117-121)
+#' found in Statistics Unlocking the Power of Data (2013)
+#' by Lock, R.H. et al.
+"week7_CocaineTrtmtExpt_2Trt_D_P"
+
+#' Cocaine Treatment Experiment (Lithium/Desipramine)
+#'
+#' Only the Lithium and Desipramine treatments.
+#'
+#' From data from Gawin, F. et al (1989)
+#' Archives of General Psychiatry 46(2) (117-121)
+#' found in Statistics Unlocking the Power of Data (2013)
+#' by Lock, R.H. et al.
+"week7_CocaineTrtmtExpt_Lith_Desip"
+
+#' Mindset Matters
+#'
+#' This data is based on an experiment where the researcher was
+#' interested in the placebo effects of mind-set on the body.
+#' 84 female work attendants from 7 large hotels were randomly
+#' allocated to one of two groups, Informed and Control.
+#' (Those with missing values have been taken out of our dataset.)
+"week7_MindsetMatters"
+
+#' Light at Night for Mice
+#'
+#' Data from an experiment with mice having different nighttime
+#' light conditions.
+#'
+#' @source \url{https://cran.r-project.org/web/packages/Lock5Data/}
+"week7_LightatNight"
+
+#' Light at Night for Mice
+#'
+#' Data from a study by Stickgold et al. (2000)
+#' Nature Neuroscience 2 (1237-8)
+#' given in Investigating Statistical Concepts Applications and
+#' Methods (2006) by Rossman and Chance
+"week7_SleepDeprivationExpt"
+
+#' Monthly Visitor Arrivals from 1998
+#'
+#' Visitor arrivals by country of residence all travel mode (Monthly)
+#'
+#' Visitor arrivals are overseas residents arriving in New Zealand
+#' for a stay of less than 12 months.
+#' For arrival series, the country of residence is the country where
+#' a person arriving in New Zealand last lived for 12 months or
+#' more (country of last permanent residence).
+#'
+#' The United Kingdom also includes the Channel Islands and the
+#' Isle of Man.
+#'
+#' Cruise passengers have only been collected separately since
+#' September 1997.
+#' Cruise passengers are only recorded as arrivals or departures
+#' if they begin or end their cruise in New Zealand.
+#' Otherwise, they are regarded as being in transit,
+#' even if they leave the ship for periods while it is in New Zealand.
+#'
+#' @source Statistics New Zealand
+"week8_Actual_VisitorArrivalsMonthlyFrom1998"
+
+#' Quarterly Visitor Arrivals from 1998
+#'
+#' Visitor arrivals by country of residence all travel mode
+#' (Quarterly)
+#'
+#' Visitor arrivals are overseas residents arriving in New Zealand
+#' for a stay of less than 12 months.
+#' For arrival series, the country of residence is the country where
+#' a person arriving in New Zealand last lived for 12 months or
+#' more (country of last permanent residence).
+#'
+#' The United Kingdom also includes the Channel Islands and the
+#' Isle of Man.
+#'
+#' Cruise passengers have only been collected separately since
+#' September 1997.
+#' Cruise passengers are only recorded as arrivals or departures
+#' if they begin or end their cruise in New Zealand.
+#' Otherwise, they are regarded as being in transit,
+#' even if they leave the ship for periods while it is in New Zealand.
+#'
+#' @source Statistics New Zealand
+"week8_Actual_VisitorArrivalsQuarterlyFrom1998"
+
+#' Average Monthly Visitor Arrivals
+#'
+#' Average number of visitors in New Zealand by country of
+#' residence (Monthly)
+#' The United Kingdom also includes the Channel Islands and the
+#' Isle of Man.
+#' The number of visitors in New Zealand is estimated from a
+#' sample of records, using day of arrival and intended length
+#' of stay. It is calculated for each day within the selected
+#' period and averaged.
+#'
+#' @source Statistics New Zealand
+"week8_AverageVisitorsMonthly"
+
+#' Average Quarterly Visitor Arrivals
+#'
+#' Average number of visitors in New Zealand by country of
+#' residence (Quarterly)
+#'
+#' The United Kingdom also includes the Channel Islands and the
+#' Isle of Man.
+#' The number of visitors in New Zealand is estimated from a
+#' sample of records, using day of arrival and intended length
+#' of stay. It is calculated for each day within the selected
+#' period and averaged.
+#'
+#' @source Statistics New Zealand
+"week8_AverageVisitorsQuarterly"
---
+++ README.md
@@ -0,0 +1,33 @@
+# FutureLearnData: R data package for the Data to Insight MOOC
+
+This package is primarly created for use with [iNZight](stat.auckland.ac.nz/~wild/iNZight).
+
+## Contents
+
+The R package contains these datasets:
+- Gapminder
+  - `gapminder`, data from 1952--2012
+  - `gapminder-2008`, data for just 2008
+- NHANES
+  - `nhanes2009_2012`, modified data from 2009--2012
+  - `nhanes_1000_50`, `nhanes_1000`, `nhanes_2000`, the first 50, 1000, and 2000 rows, respectively
+- `olympics100m`, some olympics data
+- Week 7 Datasets
+  - `CanadaPop`, an experiment
+  - `CocaineTrtmtExpt`, `CocaineTrtmtExpt-2Trt`, `CocaineTrtmtExpt-2Trt_D_P`, `CocaineTrtmtExpt_Lith_Desip`, an experiment and each two-treatment subset
+  - `LightatNight`, an experiment on mice
+  - `MindsetMatters`, an experiment on mindset effects on placebo
+  - `SleepDeprivationExpt`, an experiment on sleep deprivation
+- Week 7 Datasets (time series)
+  - `Actual_VisitorArrivalsMonthlyFrom1998`, monthly arrivals into New Zealand by country of origin
+  - `Actual_VisitorArrivalsQuarterlyFrom1998`, quarterly arrivals into New Zealand by country of origin
+  - `AverageVisitorsMonthly`, average monthly visitors by country of origin
+  - `AverageVisitorsQuarterly`, average quarterly visitors by country of origin
+
+## Usage
+
+To load, for example, the "Gapminder 2008" data set, just run these commands in R:
+```{r}
+library(FutureLearnData)
+data('gapminder-2008')
+```
